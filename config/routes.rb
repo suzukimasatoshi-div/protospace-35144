31843505b7d19f9d35ac67b79e2eee76c7730f4c
suzukimasatoshi-div <life6pride-h.m.t.r.hi.hi.hi@ezweb.ne.jp>
2021-06-05 05:41:51 +0900

    修正しました

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "prototypes#index"
 
-  resources :prototypes, only: [:index, :new, :create, :users, :show, :edit, :update, :destroy]do
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]do
   resources :comments, only: :create
   end 
   resources :users, only: :show
